dbc5280481afb8b3cfce5b511d4f14e229a9588f
Eswari Madhavarapu <eswarimadhavarapu>
2025-01-21 18:48:19 +0530

    update6

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,23 @@ const router = require('./routes')
 
 
 const app = express()
+
+const allowedOrigins = [
+    'http://localhost:3000', // Local development URL
+    process.env.FRONTEND_URL // Production frontend URL (from .env)
+  ];
+
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true); // Allow requests from allowed origins
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true, 
+    // origin : process.env.FRONTEND_URL,
+    // credentials : true
 }))
 app.use(express.json())
 app.use(cookieParser())
